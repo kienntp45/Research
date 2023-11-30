499373eb019a700800af654d0dc8c47265faea4c
kiennt3 <kiennt3@fpts.com.vn>
2023-11-30 10:04:49 +0700

    init project

---
+++ Kafka/Library.Kafka/LibraryKafka/Library.Kafka.Interface/IConsumeTask.cs
@@ -0,0 +1,8 @@
+﻿using Confluent.Kafka;
+
+namespace LibraryKafka.Library.Kafka.Interface;
+
+public interface  IConsumeTask<TKey, TValue>
+{
+    void Execute(ConsumerBuilder<TKey, TValue> build);
+}
---
+++ Kafka/Library.Kafka/LibraryKafka/Library.Kafka.Settings/AppSetting.cs
@@ -0,0 +1,10 @@
+﻿using Confluent.Kafka;
+
+namespace LibraryKafka.Library.Kafka.Settings;
+
+public class AppSetting
+{
+    public string BootstrapServers { get; init; }
+    public ProducerConfig ProducerSetting { get; init; }
+    public ConsumerConfig[] ConsumerSettings { get; init; }
+}
