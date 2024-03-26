02d689e5a10718915e58381e4dc2d732eb036ea3
kiennt3 <kiennt3@fpts.com.vn>
2024-03-26 10:01:51 +0700

    ádasd

--- Web.Api.v1/Controllers/WeatherForecastController.cs
+++ Web.Api.v1/Controllers/WeatherForecastController.cs
@@ -21,6 +21,7 @@ namespace Web.Api.v1.Controllers
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
+            _logger.LogInformation("sdsaddssds");
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
